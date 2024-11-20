613f078642e85154eed3d17e3321f17e8ae2ae1a
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-11-20 10:39:10 +0530

    added player controller as listener to light switch

--- Assets/Scripts/Interactables/LightSwitchView.cs
+++ Assets/Scripts/Interactables/LightSwitchView.cs
@@ -7,11 +7,11 @@ public class LightSwitchView : MonoBehaviour, IInteractable
     private SwitchState currentState;
 
     public delegate void LightSwitchDelegate();
-    public LightSwitchDelegate lightSwitch;
+    public static LightSwitchDelegate lightSwitch;
 
     private void OnEnable()
     {
-        lightSwitch = OnLightSwitchToggled;
+        lightSwitch += OnLightSwitchToggled;
     }
 
     private void Start() => currentState = SwitchState.Off;
