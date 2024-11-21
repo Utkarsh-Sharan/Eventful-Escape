982d9aa7c785fd0134af90bc7b74c28911711a37
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2024-11-21 11:08:06 +0530

    triggered some events for ligths turn off

--- Assets/Scripts/Events/LightsOffByGhostEvent.cs
+++ Assets/Scripts/Events/LightsOffByGhostEvent.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class LightsOffByGhostEvent : MonoBehaviour
 {
+    [SerializeField] private int _keysRequiredToTrigger;
+    [SerializeField] private SoundType _soundType;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<PlayerView>() != null && _keysRequiredToTrigger == GameService.Instance.GetPlayerController().KeysEquipped)
+        {
+            EventService.Instance.OnLightsOffByGhostEvent.InvokeEvent();
+            GetComponent<Collider>().enabled = false;
+            GameService.Instance.GetSoundView().PlaySoundEffects(_soundType);
+        }
+    }
 }
